a169475fdb40f8c1b5bbf3d0af1b037464532552
Memin37 <mehmeteminumul62@gmail.com>
2023-11-17 23:02:51 +0300

    push

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,4 +1,4 @@
-﻿ using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,24 +7,53 @@ public class Move : MonoBehaviour
     // Start is called before the first frame update
     public float moveSpeed = 5;
     public float jumpForce = 5;
+    public float dashForce = 30;
+    public float dashTime = 1;
+    public float dashCooldown = 3;
+    Animator playerAnimator;
+
+    float horizontalInput;
+    bool canDash = true;
+    bool isDashing = false;
     Rigidbody2D rb;
+    [SerializeField] private Transform GroundCheck;
+    private bool onGround;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        onGround = Physics2D.OverlapCircle(GroundCheck.position, 0.25f, LayerMask.GetMask("Ground"));
         if (true)
         {
             float horizontalInput = Input.GetAxis("Horizontal");
-            Vector2 hareket = new Vector2(horizontalInput, 0f) * moveSpeed;
-            rb.velocity = new Vector2(hareket.x, rb.velocity.y);
+            float movement = horizontalInput * moveSpeed;
+            rb.velocity = new Vector2(movement, rb.velocity.y);
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+
+        if (Input.GetKeyDown(KeyCode.Z) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+    private IEnumerator Dash() 
+    {
+        canDash = false;
+        isDashing = true;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(horizontalInput * dashForce, 0f);
+        yield return new WaitForSeconds(dashTime);
+        rb.gravityScale = 1f;
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 }
