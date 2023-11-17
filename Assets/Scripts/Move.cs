bb7d73f9004e6e54cad1253efa33df1f59e12e0d
Memin37 <mehmeteminumul62@gmail.com>
2023-11-17 21:36:19 +0300

    update

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -8,6 +8,8 @@ public class Move : MonoBehaviour
     public float moveSpeed = 5;
     public float jumpForce = 5;
     Rigidbody2D rb;
+    [SerializeField] private Transform GroundCheck;
+    private bool onGround;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -16,13 +18,14 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        onGround = Physics2D.OverlapCircle(GroundCheck.position, 0.25f, LayerMask.GetMask("Ground"));
         if (true)
         {
             float horizontalInput = Input.GetAxis("Horizontal");
-            Vector2 hareket = new Vector2(horizontalInput, 0f) * moveSpeed;
-            rb.velocity = new Vector2(hareket.x, rb.velocity.y);
+            float movement = horizontalInput * moveSpeed;
+            rb.velocity = new Vector2(movement, rb.velocity.y);
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
