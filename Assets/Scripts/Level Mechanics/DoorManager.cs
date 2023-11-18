6bbb48976bdd2ed75483e4a7495cf45caadafd42
Ebdabrarlaluh <aa.ebra.61@gmail.com>
2023-11-18 13:35:03 +0300

    update

---
+++ Assets/Scripts/Level Mechanics/DoorManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    public Animator door;
+    private bool newLevel=false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !newLevel)
+        {
+            door.SetTrigger("Open");
+            newLevel = true;
+            
+        }
+    }
+}
