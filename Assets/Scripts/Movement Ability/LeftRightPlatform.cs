16bc3bb59bfa45b1c648e9c0aff8f9e17d6f69ed
Memin37 <mehmeteminumul62@gmail.com>
2023-11-19 01:04:33 +0300

    push6

---
+++ Assets/Scripts/Movement Ability/LeftRightPlatform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftRightPlatform : MonoBehaviour
+{
+    public float hareketMesafesi = 5f; // Platformun hareket edeceği mesafe
+    public float hareketHizi = 2f; // Platformun hareket hızı
+
+    private Vector3 baslangicPozisyonu;
+
+    void Start()
+    {
+        baslangicPozisyonu = transform.position;
+    }
+
+    void Update()
+    {
+        float yeniX = Mathf.PingPong(Time.time * hareketHizi, hareketMesafesi) + baslangicPozisyonu.x;
+        transform.position = new Vector3(yeniX, transform.position.y, transform.position.z);
+    }
+}
