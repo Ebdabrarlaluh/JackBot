13d292ea727136e061d1b990dfc826f894ff24a5
Memin37 <mehmeteminumul62@gmail.com>
2023-11-18 14:43:14 +0300

    push3

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float jumpForce = 5;
+    Rigidbody2D rb;
+    Animator playerAnimator;
+    [SerializeField] private Transform GroundCheck;
+    private bool onGround;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        onGround = Physics2D.OverlapCircle(GroundCheck.position, 0.25f, LayerMask.GetMask("Ground"));
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            playerAnimator.SetTrigger("Jump");
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+}
--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,21 +4,11 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    // Start is called before the first frame update
     public float moveSpeed = 5;
-    public float jumpForce = 5;
-    public float dashForce = 30;
-    public float dashTime = 1;
-    public float dashCooldown = 3;
     Animator playerAnimator;
 
     private float horizontalInput;
-    bool canDash = true;
-    bool isDashing = false;
-    bool isMoving = false;
     Rigidbody2D rb;
-    [SerializeField] private Transform GroundCheck;
-    private bool onGround;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -28,55 +18,30 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        onGround = Physics2D.OverlapCircle(GroundCheck.position, 0.25f, LayerMask.GetMask("Ground"));
         horizontalInput = Input.GetAxis("Horizontal");
         float movement = horizontalInput * moveSpeed;
-        
+
         if (horizontalInput != 0)
-        { 
+        {
             rb.velocity = new Vector2(movement, rb.velocity.y);
             playerAnimator.SetBool("isMoving", true);
-            isMoving = true;
         }
         else
         {
             playerAnimator.SetBool("isMoving", false);
-            isMoving = false;
-        }
-        
-        if (Input.GetKeyDown(KeyCode.Space) && onGround)
-        {
-            playerAnimator.SetTrigger("Jump");
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
         if (horizontalInput > 0)
         {
             transform.localScale = new Vector3(1f, 1f, 1f); // Sağa doğru yürüyor, scale'ı orijinal hale getir
+            transform.GetChild(0).localScale = new Vector3(-1f, 1f, 1f);
         }
         else if (horizontalInput < 0)
         {
+            
             transform.localScale = new Vector3(-1f, 1f, 1f); // Sola doğru yürüyor, scale'ı -1 katı al
+            transform.GetChild(0).localScale = new Vector3(1f, 1f, 1f);
         }
-
-
-
-
-        if (Input.GetKeyDown(KeyCode.Z) && canDash)
-        {
-            StartCoroutine(Dash());
-        }
-    }
-    private IEnumerator Dash() 
-    {
-        canDash = false;
-        isDashing = true;
-        rb.gravityScale = 0f;
-        rb.velocity = new Vector2(horizontalInput * dashForce, 0f);
-        yield return new WaitForSeconds(dashTime);
-        rb.gravityScale = 1f;
-        isDashing = false;
-        yield return new WaitForSeconds(dashCooldown);
-        canDash = true;
+        
     }
 }
