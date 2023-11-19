a2082178d02840e85ee38326a6ee7c8b6fea2845
Ebdabrarlaluh <aa.ebra.61@gmail.com>
2023-11-19 15:31:39 +0300

    Spin

---
+++ Assets/Scripts/Level Mechanics/AbilityManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class AbilityManager : MonoBehaviour
+{
+    public GameObject objectWithScripts; // Kontrol edilecek obje
+    public MonoBehaviour[] allScripts; // Etkinleştirilecek betikler
+    public GameObject grap;
+
+    void Start()
+    {
+        // Öncelikle, tüm betikleri devre dışı bırak
+        allScripts = objectWithScripts.GetComponents<MonoBehaviour>();
+        foreach (MonoBehaviour script in allScripts)
+        {
+            script.enabled = false;
+        }
+    }
+    public void EnableScripts(string name)
+    {
+       
+            
+            grap.SetActive(true);
+
+            
+
+        // Ardından, sadece belirli betikleri etkinleştir
+        foreach (MonoBehaviour script in allScripts)
+        {
+            if (script.GetType().Name.ToString() == name)
+            {
+                
+                script.enabled = true;
+            }
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/Level Mechanics/spinJackBot.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class spinJackBot : MonoBehaviour
+{
+    public GameObject[] slotObjects;
+    public GameObject[] secondSlotObjects;
+    public GameObject[] activeObjects;
+    private bool onceSpin;
+
+    public AbilityManager jackpot;
+    void Start()
+    {
+        onceSpin = true;
+        foreach (GameObject obj in slotObjects)
+        {
+            obj.SetActive(false);
+        }
+        foreach (GameObject obj in secondSlotObjects)
+        {
+            obj.SetActive(false);
+        }
+
+        StartCoroutine(ShowSlotObjectsSequentially());
+    }
+
+    IEnumerator ShowSlotObjectsSequentially()
+    {
+        while (onceSpin)
+        {
+            // Slot objelerini teker teker gösterme
+            foreach (GameObject obj in slotObjects)
+            {
+                obj.SetActive(true);
+                yield return new WaitForSeconds(1f); obj.SetActive(false);
+            }
+
+            foreach (GameObject obj in slotObjects)
+            {
+                obj.SetActive(true);
+                yield return new WaitForSeconds(0.5f);
+                obj.SetActive(false);
+            }
+            foreach (GameObject obj in slotObjects)
+            {
+                obj.SetActive(true);
+                yield return new WaitForSeconds(0.25f);
+                obj.SetActive(false);
+            }
+            foreach (GameObject obj in slotObjects)
+            {
+                obj.SetActive(true);
+                yield return new WaitForSeconds(0.1f);
+                obj.SetActive(false);
+            }
+            foreach (GameObject obj in slotObjects)
+            {
+                obj.SetActive(true);
+                yield return new WaitForSeconds(0.05f); obj.SetActive(false);
+            }
+
+            int randomIndex = Random.Range(0, slotObjects.Length);
+            activateScripts(slotObjects[randomIndex]);
+            slotObjects[randomIndex].SetActive(true);
+            yield return new WaitForSeconds(1f);
+
+            
+
+            /////////////////////////////////////////////////////////////
+
+            foreach (GameObject obj in secondSlotObjects)
+            {
+                obj.SetActive(true);
+                yield return new WaitForSeconds(1f);
+                obj.SetActive(false);
+            }
+
+            foreach (GameObject obj in secondSlotObjects)
+            {
+                obj.SetActive(true);
+                yield return new WaitForSeconds(0.5f);
+                obj.SetActive(false);
+            }
+            foreach (GameObject obj in secondSlotObjects)
+            {
+                obj.SetActive(true);
+                yield return new WaitForSeconds(0.25f); obj.SetActive(false);
+            }
+            foreach (GameObject obj in secondSlotObjects)
+            {
+                obj.SetActive(true);
+                yield return new WaitForSeconds(0.1f); obj.SetActive(false);
+            }
+            foreach (GameObject obj in secondSlotObjects)
+            {
+                obj.SetActive(true);
+                yield return new WaitForSeconds(0.05f);
+                obj.SetActive(false);
+            }
+
+            int secondRandomIndex = Random.Range(0, secondSlotObjects.Length);
+            activateScripts(secondSlotObjects[secondRandomIndex]);
+            secondSlotObjects[secondRandomIndex].SetActive(true);
+            yield return new WaitForSeconds(3f);
+            onceSpin = false;
+        }
+    }
+
+    void activateScripts(GameObject activeObject)
+    {
+        string x = activeObject.name;
+
+        if (x == "Move")
+        {
+            jackpot.EnableScripts("Jump");
+        }
+        if (x == "Dash")
+        {
+            jackpot.EnableScripts("Ghost Effect");
+        }
+
+        jackpot.EnableScripts(x);
+    }
+}
