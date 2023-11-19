4f61bfa9b1456e43ff21098677e2b23162f59af2
Memin37 <mehmeteminumul62@gmail.com>
2023-11-19 15:55:43 +0300

    push8

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,55 +5,99 @@ using UnityEditor.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
-    Rigidbody2D rb;
-    public float moveSpeed = 5f;
-    bool goRight;
+    public float heightIncrease = 2f; 
+    public float levitateTime = 3f;
+    public Animator animator;
+
+    //bool goRight;
     public float flyTime = 1f;
-    float remainTime;
+    //float remainTime;
+    private Vector3 oldPosition;
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        remainTime = flyTime;
+        //remainTime = flyTime;
+        oldPosition = transform.position;
+        
     }
     void FixedUpdate()
     {
-        if (goRight)
-        {
-            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
-            transform.localScale = new Vector3(-1, 1, 1); // Sağa baktığınızı temin etmek için scale'ı ayarlayın
-        }
-        else
-        {
-            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
-            transform.localScale = new Vector3(1, 1, 1); // Sola baktığınızı temin etmek için scale'ı ayarlayın
-        }
-        remainTime -= Time.deltaTime;
-        if (remainTime <= 0f)
-        {
-            goRight = !goRight; // Süre dolduğunda yönü değiştir
-            remainTime = flyTime; // Bekleme süresini yeniden ayarla
-        }
+        //if (goRight)
+        //{
+        //    transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        //    transform.localScale = new Vector3(-1, 1, 1); // Sağa baktığınızı temin etmek için scale'ı ayarlayın
+        //}
+        //else
+        //{
+        //    transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        //    transform.localScale = new Vector3(1, 1, 1); // Sola baktığınızı temin etmek için scale'ı ayarlayın
+        //}
+        //remainTime -= Time.deltaTime;
+        //if (remainTime <= 0f)
+        //{
+        //    goRight = !goRight; // Süre dolduğunda yönü değiştir
+        //    remainTime = flyTime; // Bekleme süresini yeniden ayarla
+        //}
     }
 
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.CompareTag("Player"))
+        if (col.CompareTag("Projectile"))
+        {
+            StartCoroutine(Levitate());
+            Destroy(col.gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
         {
             Destroy(col.gameObject);
             EditorSceneManager.LoadScene("SampleScene");
         }
-
-        if (col.CompareTag("Projectile"))
-        {
-            StartCoroutine(Levitate());
-        }
-
     }
     private IEnumerator Levitate() 
     {
-        rb.gravityScale = -5;
-        yield return new WaitForSeconds(2f);
-        rb.gravityScale = 2;
+
+        
+        Vector3 targetHeight = transform.position + Vector3.up * heightIncrease; // Yükseklik artışının hedef pozisyonu
+
+        float startTime = Time.time; // Başlangıç zamanını kaydet
+        float passingTime = 0f; // Geçen süreyi sakla
+
+        while (passingTime < levitateTime)
+        {
+            passingTime = Time.time - startTime; // Geçen süreyi hesapla
+            float riseRatio = Mathf.Clamp01(passingTime / levitateTime); // Yukarı çıkma oranı
+            animator.SetBool("levitate", true);
+            // Yavaşça yukarı çıkma
+            transform.position = Vector3.Lerp(oldPosition, targetHeight, riseRatio);
+            
+            yield return null;
+        }
+
+        
+        yield return new WaitForSeconds(1f);
+
+        //// Aşağı inme işlemi
+        //startTime = Time.time; // Yeniden başlangıç zamanını kaydet
+        //passingTime = 0f; // Geçen süreyi sıfırla
+
+        //while (passingTime < levitateTime)
+        //{
+        //    passingTime = Time.time - startTime; // Geçen süreyi hesapla
+        //    float fallRatio = Mathf.Clamp01(passingTime / levitateTime); // Aşağı inme oranı
+
+        //    // Yavaşça aşağı inme
+        //    transform.position = Vector3.Lerp(targetHeight, oldPosition, fallRatio);
+
+        //    yield return null;
+        //}
+
+
+
+        oldPosition = transform.position;
+        animator.SetBool("levitate", false);
+
     }
 }
