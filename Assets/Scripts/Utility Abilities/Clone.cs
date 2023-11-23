198b42b50336209e00860cc7baf4806361440b5b
Memin37 <mehmeteminumul62@gmail.com>
2023-11-24 00:43:55 +0300

    push15

--- Assets/Scripts/Utility Abilities/Clone.cs
+++ Assets/Scripts/Utility Abilities/Clone.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class Clone : MonoBehaviour
 {
+    public float moveSpeed = 15f;
     public float cloneDelay;
     private float cloneDelaySeconds;
     public GameObject clone;
+    Vector2 moveDirection = Vector2.right;
+    SpriteRenderer cloneSprite;
 
     public KeyCode cloneKeyCode=KeyCode.K;
     void Start()
     {
         cloneDelaySeconds = 0;
+        cloneSprite = clone.GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float hareketYonu = Input.GetAxis("Horizontal");
+
+        if (hareketYonu > 0)
+        {
+            moveDirection = Vector2.right;
+            cloneSprite.flipX = false;
+        }
+        else if (hareketYonu < 0)
+        {
+            moveDirection = Vector2.left;
+            cloneSprite.flipX = true;
+        }
+        
         if (cloneDelaySeconds > 0)
         {
             cloneDelaySeconds -= Time.deltaTime;
@@ -25,7 +41,9 @@ public class Clone : MonoBehaviour
         {
             if (Input.GetKeyDown(cloneKeyCode))
             {
-                Instantiate(clone, new Vector2(transform.position.x+1,transform.position.y), transform.rotation);
+                GameObject newClone = Instantiate(clone, new Vector2(transform.position.x + moveDirection.x, transform.position.y), transform.rotation);
+                Rigidbody2D rb = newClone.GetComponent<Rigidbody2D>();
+                rb.velocity = moveDirection * moveSpeed;
                 cloneDelaySeconds = cloneDelay;
             }
         }
